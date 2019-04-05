6d7c9cf87ddc8f4cdb71c68494919f5f54f73d82
juanantonioCB <juanantonio186@hotmail.com>
2019-04-05 13:59:37 +0200

    Delete Programa.java

--- Programa.java
+++
@@ -1,9 +0,0 @@
-import clases.*;
-public class Programa{
-	public static void main(String[] args) {
-		Ave ave1=new Ave("no se","jose",5,"macho");
-		Ave ave2=new Ave("no se","jose",3,"ssdff");
-		System.out.println(ave1);
-		System.out.println(ave2);
-	}
-}
